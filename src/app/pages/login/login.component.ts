3eadbf041b986ac2d96b70c36bfbdf8cb80e64e0
USpiri <urielspiridione135@gmail.com>
2023-11-21 18:37:04 -0300

    ✨ Feat: Loggin message

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -18,6 +18,12 @@ import { RouterLink } from '@angular/router';
           <h1 class="text-center text-2xl">Roles con Auth0</h1>
           <app-button (btnClick)="login()">Iniciar sesión</app-button>
         </div>
+        <p>
+          <strong>Nota:</strong> Las demás rutas estarán bloqueadas a menos que
+          se inicie sesión, y algúnas de ellas estaran bloqueadas a menos que se
+          tenga un "rol" determinado. Puedes intentar redirigirte a la página de
+          inicio, pero sin éxito alguno.
+        </p>
         <a
           routerLink="/home"
           class="underline hover:scale-[102%] hover:font-semibold transition-all"
