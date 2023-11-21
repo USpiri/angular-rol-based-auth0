0e189b13638409e5b927ff81f7956fb379fee245
USpiri <urielspiridione135@gmail.com>
2023-11-21 20:29:16 -0300

    🔧 Config: Use provideAuth0 fn

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,18 +1,11 @@
-import {
-  mergeApplicationConfig,
-  ApplicationConfig,
-  importProvidersFrom,
-} from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { AuthModule } from '@auth0/auth0-angular';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { environment } from 'src/environments/environment';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    importProvidersFrom(AuthModule.forRoot({ ...environment.auth0 })),
-  ],
+  providers: [provideServerRendering(), provideAuth0({ ...environment.auth0 })],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { AuthModule } from '@auth0/auth0-angular';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { environment } from 'src/environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    importProvidersFrom(AuthModule.forRoot({ ...environment.auth0 })),
+    provideAuth0({ ...environment.auth0 }),
   ],
 };
