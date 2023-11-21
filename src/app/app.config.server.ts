5f56f518f1a1bd4a1611f817f0e64d0c3756a0c5
USpiri <urielspiridione135@gmail.com>
2023-11-21 18:53:07 -0300

    ✨ Feat: Bootstrap auth module

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,18 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import {
+  mergeApplicationConfig,
+  ApplicationConfig,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment } from 'src/environments/environment';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
-  ]
+    provideServerRendering(),
+    importProvidersFrom(AuthModule.forRoot({ ...environment.auth0 })),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
