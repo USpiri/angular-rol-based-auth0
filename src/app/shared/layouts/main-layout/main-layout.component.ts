3e98d929bb1722af0d2a9c0ad6e93ee904f5d4c4
USpiri <urielspiridione135@gmail.com>
2023-11-21 17:16:16 -0300

    ✨ Feat: Layout Component

---
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent, RouterOutlet],
+  template: `
+    <div class="h-screen">
+      <app-navbar></app-navbar>
+      <router-outlet></router-outlet>
+    </div>
+  `,
+})
+export class MainLayoutComponent {}
