0edab3ecf7fa8b58ad8715ec0496ff513afc5ae7
USpiri <urielspiridione135@gmail.com>
2023-11-21 17:15:22 -0300

    ✨ Feat: Link Button component

---
+++ src/app/shared/components/link-btn/link-btn.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-link-btn',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  template: `
+    <a
+      [routerLink]="routerLink"
+      class="py-2 block px-3 w-full bg-neutral-800 hover:bg-neutral-700 transition-all rounded-md active:bg-neutral-800 active:scale-95 duration-100"
+    >
+      <ng-content></ng-content>
+    </a>
+  `,
+})
+export class LinkBtnComponent {
+  @Input() routerLink!: string;
+}
